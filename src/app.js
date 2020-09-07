1456904c08f0a20e34a483cf4d5ee8b0b7b518f5
Izaac Abrams <52928596+izaacAbrams@users.noreply.github.com>
2020-09-07 14:19:45 -0500

    added track title

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-const path = require('path')
-const fs = require('fs')
-const ytdl = require('ytdl-core')
+const path = require("path");
+const fs = require("fs");
+const ytdl = require("ytdl-core");
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
@@ -16,28 +16,29 @@ app.use(helmet());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+	res.send("Hello, world!");
 });
 
-app.get('/download', (req,res) => {
-  const URL = req.query.URL;
-  res.header('Content-Disposition', 'attachment; filename="music.mp3"');
-  ytdl(URL, {
-      format: 'mp3'
-      }).pipe(res);
-  });
+app.get("/download", (req, res) => {
+	const URL = req.query.URL;
+	const title = req.query.title;
+	res.header("Content-Disposition", `attachment; filename=${title}.mp3`);
+	ytdl(URL, {
+		format: "mp3",
+	}).pipe(res);
+});
 
-app.get('/info', (req, res) => {
-  ytdl.getBasicInfo(req.query.URL).then(info => res.json(info))
-})
+app.get("/info", (req, res) => {
+	ytdl.getBasicInfo(req.query.URL).then((info) => res.json(info));
+});
 app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.log(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
+	let response;
+	if (NODE_ENV === "production") {
+		response = { error: { message: "server error" } };
+	} else {
+		console.log(error);
+		response = { message: error.message, error };
+	}
+	res.status(500).json(response);
 });
 module.exports = app;
